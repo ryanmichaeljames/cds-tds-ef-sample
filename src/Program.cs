a3c9c9969075cbf9b65e267c44af166fb230ded9
Ryan James <ryanmichaeljames@gmail.com>
2020-05-30 22:26:54 +1200

    Initial commit

---
+++ src/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+                var connectionString = Configuration.GetConnectionString("cds");
+
+                Console.ReadKey();
+        }
+    }
+}
